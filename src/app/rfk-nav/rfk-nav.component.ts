e8dd4b563c9d5fe79626556fdbeabd3c79287b18
Alexander Pehm <alexander@pehm.biz>
2021-08-18 17:00:56 +0200

    Material sidenav layout

---
+++ src/app/rfk-nav/rfk-nav.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-rfk-nav',
+  templateUrl: './rfk-nav.component.html',
+  styleUrls: ['./rfk-nav.component.css']
+})
+export class RfkNavComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
