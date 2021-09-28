a11557157c514e9691715be1d4b21bd4b00f6dfe
Alexander Pehm <alexander@pehm.biz>
2021-09-28 17:38:58 +0200

    fix env file generation to include audience

--- src/assets/scripts/setEnv.ts
+++ src/assets/scripts/setEnv.ts
@@ -46,7 +46,8 @@ const environmentFileContent = `
   export const environment = {
     production: ${isProduction},
     AUTH0_CLIENTID: '${process.env.AUTH0_CLIENTID}',
-    AUTH0_DOMAIN: '${process.env.AUTH0_DOMAIN}'
+    AUTH0_DOMAIN: '${process.env.AUTH0_DOMAIN}',
+    AUTH0_AUDIENCE: '${process.env.AUTH0_AUDIENCE}'
   };
 `;
 
